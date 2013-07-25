7bf5e5637ee89b7cf64ed251c4d89e0ac4244dc2
qluo1 <qi.dataingelligence@gmail.com>
2013-07-25 03:05:48 +0100

    working hw1, finished prob 1

--- labhw2/hw2.py
+++ labhw2/hw2.py
@@ -43,36 +43,35 @@ def vec_select_sum(veclist, k, D):
     return vec_sum(vec_select(veclist,k),D)
 
 
-# ## Problem 2
-# def scale_vecs(vecdict):
-#     '''
-#     >>> v1 = Vec({1,2,3}, {2: 9})
-#     >>> v2 = Vec({1,2,4}, {1: 1, 2: 2, 4: 8})
-#     >>> scale_vecs({3: v1, 5: v2}) == [Vec({1,2,3},{2: 3.0}), Vec({1,2,4},{1: 0.2, 2: 0.4, 4: 1.6})]
-#     True
-#     '''
-#     pass
+## Problem 2
+def scale_vecs(vecdict):
+    '''
+    >>> v1 = Vec({1,2,3}, {2: 9})
+    >>> v2 = Vec({1,2,4}, {1: 1, 2: 2, 4: 8})
+    >>> scale_vecs({3: v1, 5: v2}) == [Vec({1,2,3},{2: 3.0}), Vec({1,2,4},{1: 0.2, 2: 0.4, 4: 1.6})]
+    True
+    '''
+    return [1.0/k*vecdict[k] for k in vecdict]
 
 
-
-# ## Problem 3
-# def GF2_span(D, L):
-#     '''
-#     >>> from GF2 import one
-#     >>> D = {'a', 'b', 'c'}
-#     >>> L = [Vec(D, {'a': one, 'c': one}), Vec(D, {'b': one})]
-#     >>> len(GF2_span(D, L))
-#     4
-#     >>> Vec(D, {}) in GF2_span(D, L)
-#     True
-#     >>> Vec(D, {'b': one}) in GF2_span(D, L)
-#     True
-#     >>> Vec(D, {'a':one, 'c':one}) in GF2_span(D, L)
-#     True
-#     >>> Vec(D, {x:one for x in D}) in GF2_span(D, L)
-#     True
-#     '''
-#     pass
+## Problem 3
+def GF2_span(D, L):
+    '''
+    >>> from GF2 import one
+    >>> D = {'a', 'b', 'c'}
+    >>> L = [Vec(D, {'a': one, 'c': one}), Vec(D, {'b': one})]
+    >>> len(GF2_span(D, L))
+    4
+    >>> Vec(D, {}) in GF2_span(D, L)
+    True
+    >>> Vec(D, {'b': one}) in GF2_span(D, L)
+    True
+    >>> Vec(D, {'a':one, 'c':one}) in GF2_span(D, L)
+    True
+    >>> Vec(D, {x:one for x in D}) in GF2_span(D, L)
+    True
+    '''
+    pass
 
 
 
