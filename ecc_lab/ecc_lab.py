ba8803ca085bbcac485c59ff561bc62a9392ea7a
luoqi <luoqi@yahoo.com>
2013-08-06 22:25:43 +1000

    update

--- ecc_lab/ecc_lab.py
+++ ecc_lab/ecc_lab.py
@@ -3,18 +3,24 @@ from mat import Mat
 from bitutil import noise
 from GF2 import one
 from matutil import *
+from vecutil import *
 
 ## Task 1 part 1
 """ Create an instance of Mat representing the generator matrix G. You can use
 the procedure listlist2mat in the matutil module (be sure to import first).
 Since we are working over GF (2), you should use the value one from the
 GF2 module to represent 1"""
-G = listlist2mat([[one,0,0,0],[0,one,0,0],[0,0,one,0],[0,0,0,one]])
+G = listlist2mat([[one,0,one,one],\
+                  [one,one,0,one],\
+                  [0,0,0,one],\
+                  [one,one,one,0],\
+                  [0,0,one,0],\
+                  [0,one,0,0],\
+                  [one,0,0,0]])
 
 ## Task 1 part 2
 # Please write your answer as a list. Use one from GF2 and 0 as the elements.
-encoding_1001 = 
-
+encoding_1001 = (G* list2vec([one,0,0,one])).tolist()
 
 ## Task 2
 # Express your answer as an instance of the Mat class.
@@ -42,9 +48,9 @@ def find_error(e):
 ## Task 4 part 2
 # Use the Vec class for your answers.
 non_codeword = Vec({0,1,2,3,4,5,6}, {0: one, 1:0, 2:one, 3:one, 4:0, 5:one, 6:one})
-error_vector = Vec(..., ...)
-code_word = Vec(..., ...)
-original = ... # code_word * R
+# error_vector = Vec(..., ...)
+# code_word = Vec(..., ...)
+# original = ... # code_word * R
 
 
 ## Task 5
